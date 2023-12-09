0877e25435989500dc0eb125460f6c060a22018d
sidiAbdalla <sidabdallard@gmail.com>
2023-12-09 22:56:14 +0100

    Sat Dec  9 10:56:14 PM +01 2023

--- src/app.py
+++ src/app.py
@@ -1,10 +1,8 @@
-from pyspark.sql import SparkSession
-from pyspark import SparkContext
-sc = SparkContext.getOrCreate()
-
-
-spark = SparkSession.builder.appName("PySPARK").master("local[*]").getOrCreate(sc)
-
-
-
-spark.stop()
+# projctesDf = spark.read.format("jdbc").option("driver", "com.mysql.cj.jdbc.Driver") \
+#     .option("url", "jdbc:mysql://localhost:3306/DB_IMOMAROC") \
+#     .option("user", "root").option("password", "1234@@@@") \
+#     .option("query","select P.ID_PROJECT , P.TITRE,count(T.ID_TACHES) as NUMBER from Project  as P "
+#                     "INNER JOIN TACHES AS T ON P.ID_PROJECT = T.ID_PROJECT"
+#                     " WHERE DATEDIFF(T.DATE_FIN, T.DATE_DEBIT) > 30 "
+#                     ""
+#                     "group by P.ID_PROJECT ").load()
